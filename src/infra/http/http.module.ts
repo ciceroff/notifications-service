2e090ca5e46ca21f26faf3f0674fe118c77a6ca9
ciceroff <cicero.barrozo13@gmail.com>
2023-02-14 17:07:37 -0300

    🐛 add http module providers

--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -1,4 +1,7 @@
 import { Module } from '@nestjs/common';
+import { CancelNotification } from 'src/app/use-cases/cancel-notification';
+import { CountRecipientNotification } from 'src/app/use-cases/count-recipient-notifications';
+import { GetRecipientNotification } from 'src/app/use-cases/get-recipient-notifications';
 import { SendNotification } from 'src/app/use-cases/send-notification';
 import { DatabaseModule } from '../database/database.module';
 import { NotificationsController } from './controllers/notifications.controller';
@@ -6,6 +9,11 @@ import { NotificationsController } from './controllers/notifications.controller'
 @Module({
   imports: [DatabaseModule],
   controllers: [NotificationsController],
-  providers: [SendNotification],
+  providers: [
+    SendNotification,
+    CancelNotification,
+    CountRecipientNotification,
+    GetRecipientNotification,
+  ],
 })
 export class HttpModule {}
