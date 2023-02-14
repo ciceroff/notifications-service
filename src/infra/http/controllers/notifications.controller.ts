0b9f614c2a582a0a7ba48f13a935cf0f453b01b3
ciceroff <cicero.barrozo13@gmail.com>
2023-02-14 16:54:47 -0300

    ♻️ add endpoints for notifications controllers

--- src/infra/http/controllers/notifications.controller.ts
+++ src/infra/http/controllers/notifications.controller.ts
@@ -1,11 +1,31 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Patch, Post } from '@nestjs/common';
+import { CancelNotification } from 'src/app/use-cases/cancel-notification';
+import { CountRecipientNotification } from 'src/app/use-cases/count-recipient-notifications';
 import { SendNotification } from 'src/app/use-cases/send-notification';
 import { CreateNotificationBody } from '../dtos/create-notification-body';
 import { NotificationViewModel } from '../view-models/notification-view-model';
 
 @Controller('notifications')
 export class NotificationsController {
-  constructor(private sendNotification: SendNotification) {}
+  constructor(
+    private sendNotification: SendNotification,
+    private cancelNotification: CancelNotification,
+    private countRecipientNotifications: CountRecipientNotification,
+  ) {}
+
+  @Patch(':id/cancel')
+  async cancel(@Param('id') id: string) {
+    await this.cancelNotification.execute({ notificationId: id });
+  }
+
+  @Get(':id/count')
+  async countFromRecipient(@Param('id') id: string) {
+    const { count } = await this.countRecipientNotifications.execute({
+      recipientId: id,
+    });
+
+    return count;
+  }
   @Post()
   async create(@Body() body: CreateNotificationBody) {
     const { category, content, recipientId } = body;
