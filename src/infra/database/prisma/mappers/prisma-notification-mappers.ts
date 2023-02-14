de97bf42b9aba6ce0560c007e4d3592edfe756c5
ciceroff <cicero.barrozo13@gmail.com>
2023-02-14 16:54:32 -0300

    ✨ add prisma repositories methods and mappers

--- src/infra/database/prisma/mappers/prisma-notification-mappers.ts
+++ src/infra/database/prisma/mappers/prisma-notification-mappers.ts
@@ -1,5 +1,6 @@
 import { Notification } from 'src/app/entities/notification';
-
+import { Notification as RawNotification } from '@prisma/client';
+import { Content } from 'src/app/entities/content';
 export class PrismaNotificationMapper {
   static toPrisma(notification: Notification) {
     return {
@@ -10,5 +11,19 @@ export class PrismaNotificationMapper {
       readAt: notification.readAt,
       createdAt: notification.createdAt,
     };
+  }
+
+  static toDomain(raw: RawNotification) {
+    return new Notification(
+      {
+        category: raw.category,
+        content: new Content(raw.content),
+        readAt: raw.readAt,
+        canceledAt: raw.canceledAt,
+        createdAt: raw.createdAt,
+        recipientId: raw.recipientId,
+      },
+      raw.id,
+    );
   }
 }
